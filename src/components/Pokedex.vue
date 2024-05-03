085df8a7725885d3a159948017d98b0ed81079e6
jiajiechen2003 <jchen2223@politecnics.barcelona>
2024-05-03 18:09:41 +0200

    cards

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,19 +1,3 @@
-<template>
-    <div>
-        <div v-if="pokemons.length > 0">
-            <h2>Lista de nombres de Pokémon:</h2>
-            <ul>
-                <li v-for="pokemon in pokemons" :key="pokemon.name">
-                    <strong>{{ pokemon.name }}</strong> - Habilidades:
-                    <ul>
-                        <li v-for="ability in pokemon.abilities" :key="ability.ability.name">{{ ability.ability.name }}</li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</template>
-
 <script>
 // import axios from 'axios';
 
@@ -47,6 +31,54 @@ export default {
 }
 </script>
 
+<template>
+    <!-- <div>
+        <div v-if="pokemons.length > 0">
+            <h2>Lista de nombres de Pokémon:</h2>
+            <ul>
+                <li v-for="pokemon in pokemons" :key="pokemon.name">
+                    <strong>{{ pokemon.name }}</strong> - Habilidades:
+                    <ul>
+                        <li v-for="type in pokemon.types" :key="type.type.name">{{ type.type.name }}</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div> -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4 card-col" v-for="pokemon in pokemons" :key="pokemon.name">
+                <div class="card pokemon-card">
+                    <div class="card-header">{{ pokemon.name }}</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
 <style scoped>
-/* Estilos específicos del componente */
+.container-fluid {
+    height: auto;
+    background-color: red;
+}
+
+
+.card-col{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+}
+
+.pokemon-card {
+    width: 320px;
+    height: 400px;
+}
 </style>
